d657d436c3bd10a22ab1643822d4f19f51f7d575
Vladimir <vbolotovv@yandex.ru>
2024-01-31 12:01:04 +0300

    new test

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main() {
-	cout << "Hallo world!";
+	vector <int> data(20);
+	int curpos = 0;
+	int num;
+	cin >> num;
+	while (num != -2){
+		if (num == -1){
+			for (int i = curpos; i < data.size(); ++i){
+				cout << data[i] << " ";
+			}
+			for (int i = 0; i < curpos; ++i){
+				cout << data[i] << " ";
+			} 
+		} else {
+			data[curpos] = num;
+			curpos++;
+			if (curpos == 20) curpos = 0;
+
+		}
+		cin >> num;
+	}
 	return 0;
 }
