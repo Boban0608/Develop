abf220f6243da28ab04e84c76b1fe1d23a9d65f6
Vladimir <vbolotovv@yandex.ru>
2024-02-20 15:50:28 +0300

    Link 1

---
+++ C++/Skillbox/l18.5_1.cpp
@@ -0,0 +1,52 @@
+/*
+Напишите функцию swapvec, принимающую std::vector<int> и указатель на массив переменных типа 
+int (одного размера) и обменивающую значения этих массивов.
+Пример:
+std::vector<int> a = {1,2,3,4};
+int b[] = {2,4,6,8};
+swapvec(a,b);
+for(int i = 0; i < 4; ++i)
+
+   std::cout << a[i];
+
+std::cout << std::endl;
+for(int i = 0; i < 4; ++i)
+
+   std::cout << b[i];
+
+
+Вывод:
+2468
+1234
+Чек-лист для проверки задачи
+Функция принимает корректные типы данных, тип возвращаемого значения — void.
+Функция не использует библиотек кроме <iostream> и <vector>.
+Функция меняет значения между объектами.
+Вектор передаётся по ссылке.
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+void swapvec(vector <int>& a, int (&b)[4]){
+    for (int i = 0; i < 4; i++){
+        int tmp = a[i];
+        a[i] = b[i];
+        b[i] = tmp;
+    }    
+}
+main() {
+  cout << "----------------\n";
+  cout << "Link 1\n";
+  cout << "----------------\n";
+  vector <int> a = {1,2,3,4};
+  int b[] = {2,4,6,8};
+  swapvec(a, b);
+  for(int i = 0; i < 4; ++i){
+    std::cout << a[i];
+  }
+  std::cout << std::endl;
+  for(int i = 0; i < 4; ++i){
+    std::cout << b[i];
+  }   
+  return 0; 
+}
