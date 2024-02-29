acef3013c6b954d619e8698366471e38b913d247
Vladimir <vbolotovv@yandex.ru>
2024-02-29 10:33:52 +0300

    last test

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,27 +1,39 @@
 #include <iostream>
-#include <vector>
+#include <fstream>
 using namespace std;
 
 int main() {
-	vector <int> data(20);
-	int curpos = 0;
-	int num;
-	cin >> num;
-	while (num != -2){
-		if (num == -1){
-			for (int i = curpos; i < data.size(); ++i){
-				cout << data[i] << " ";
-			}
-			for (int i = 0; i < curpos; ++i){
-				cout << data[i] << " ";
-			} 
-		} else {
-			data[curpos] = num;
-			curpos++;
-			if (curpos == 20) curpos = 0;
-
-		}
-		cin >> num;
+	string test;
+	string out;
+	cout << "Input -\n";
+	ofstream teststream("test.bin", ios::binary);
+	for (int i = 0; i < 6; i++){
+		int len;
+		test = "test";
+		len = test.length();
+		cout << test << endl;
+		teststream.write((char*) &len, sizeof(len));
+		teststream.write((char*) test.c_str(), len);		
 	}
+	
+	for (int i = 0; i < 6; i++){
+		test = "Good";
+		int len;
+		cout << test << endl;
+		len = test.length();
+		teststream.write((char*) &len, sizeof(len));
+		teststream.write((char*) test.c_str(), len);
+	}
+	teststream.close();
+	ifstream teststream1("test.bin", ios::binary);
+	cout << "Output - \n";
+	while(!teststream1.eof()){	
+	  	int len;
+		teststream1.read((char*) &len, sizeof(len));
+		out.resize(len);
+		teststream1.read((char*) out.c_str(), len);
+		cout << out << endl;
+	}
+	teststream1.close();
 	return 0;
 }
